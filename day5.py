342413f98bbcb9e8d76313c00c8da1569ec21b9c
Bantu_Deepika <166101116+Bantudeepika9391@users.noreply.github.com>
2024-06-26 20:27:03 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,144 @@
+function prog
+:its is style of writing
+frist order function :
+*pass fun as a parameter
+*func can stored to a var
+*fun can be returned"""
+def add():
+    print("adding")
+print(x()) and
+
+====high order function:
+func which accepts func as a parameter
+def printTest(a):
+ print(a)
+def test(w):
+ w("deepika")
+test(printTest)
+===pira:
+*fixed set input gives fixed set of output
+*godal function
+
+calss car:
+    var1=10
+    @classmethod
+    def test(cls):
+        car.
+
+
+
+
+code: not a pure because the code is outside the function
+    def add(x,y):
+        return x+y
+    x=3
+    y=4
+    x=add(x,y)
+
+
+:creating new func is better then modify the function
+    a="hi"
+    a="heyy"#
+
+===code ==
+num=list(map(int,input().split(" ,")))
+sq=[]
+ y=Math.sqrt(num)
+ y.append(sq)
+ =======
+ a=[1, 2, 3, 5]
+  def sq(i):
+      return i*i
+  print(list(map(sq,a)))  #write fun for one number then map will return for all the function
+
+
+==============
+a=["hi , priya"]
+ def cap(x):
+     #return x.ToCapitalize(a)
+      return i[0]+i[1].upper()+i[2:]
+      
+print(list(map(cap ,a)))
+
+
+===filter and check condition and make new list
+*map work for all value
+*filter will not work for all it will check the condition
+a=[1, 2, , 3, 5, 6]
+def f(i):
+    return i>20
+print(list(filter(f,a)))
+
+
+
+===== even==
+a=[90.89,78,456,34]
+def f(i):
+    return i%2==0
+print(list(filter(f,a)))
+======
+a=['im','am','deepika']
+def l(i):
+    return len(i)==2 and i[0]=='T'
+print(list(filter(l,a)))
+
+
+====reduced=
+some of elements in the list we grt only one value
+a=[4,7,8,9]
+def f(i):
+    return
+
+
+def add(x,y)
+#defult value will take the first value y will take the all the values
+ x=x+y
+return x
+reduce(add,a)
+===========
+from function import reduce
+a=["hello","world","python","is","great"]
+def f(i):
+    x=x+y
+    return x
+reduce(add,a)
+===========
+a=["hello","world","python","is","great"]
+from functool import reduce
+def joining(x,y):
+  return x+" "+y
+reduce(joining,a,'0')
+print(
+
+======
+zip=dict
+
+a=[1,2,3,4]
+b=[4,7,8]
+(1,4)(2,6)(3,9)
+print(list(zip(a,b,c,d)))
+===========
+creat a new list from a list where only even sq of the list be stored
+a=[1,3,2,5,8,4]
+def f(i):
+   return i*i and i<50
+print(list(filter(f,a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
